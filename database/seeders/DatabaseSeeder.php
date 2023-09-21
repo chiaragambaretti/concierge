264a20e8e3154e9b82375cd0cef70f39844e5ff2
chiaragambaretti <gambarettichiara@gmail.com git config --global user.name chiaragambaretti git config --global user.email gambarettichiara@gmail.com>
2023-09-21 15:22:06 +0200

    modifiche

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\RoomSeeder;
+use Database\Seeders\ImageSeeder;
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\SubcategoriesSeeder;
+use Database\Seeders\SubCategoryImageSeeder;
 
 class DatabaseSeeder extends Seeder
 {
